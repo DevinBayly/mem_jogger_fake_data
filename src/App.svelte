3433b341cbc5d30dfb35da6d9f9a99f371949570
DevinBayly <baylyd@email.arizona.edu>
2020-06-16 17:27:36 -0700

    seems to be a prob with the esri tile layer and its encoding of points, normal map is fine

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   export let name;
   import { onMount } from "svelte";
-  import * as d3 from "d3"
+  import * as d3 from "d3";
   onMount(() => {
     console.log("loaded");
-    let mymap = L.map("mapid").setView([32.231481, -110.951838],18);
+    /*
+    let mymap = L.map("mapid").setView([32.231481, -110.951838], 18);
     L.esri.basemapLayer("Gray").addTo(mymap);
     var subtleUA = L.esri
       .tiledMapLayer({
@@ -12,17 +13,29 @@
           "https://services.maps.arizona.edu/pdc/rest/services/SubtleCanvasTiles/MapServer"
       })
       .addTo(mymap);
-    // get bounds 
-    console.log(mymap.getBounds(),"those are lat lngs",mymap.getPixelBounds(),"these are pixel bounds")
+      */
+    let mymap = L.map("mapid").setView([32.231481, -110.951838], 17);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    // get bounds
+    console.log(
+      mymap.getBounds(),
+      "those are lat lngs",
+      mymap.getPixelBounds(),
+      "these are pixel bounds"
+    );
     // this is the width the svg should be to cover the full map
-    let bounds = mymap.getPixelBounds()
-    let width = bounds.max.x- bounds.min.x
-    let height = bounds.max.y - bounds.min.y
-    console.log("origin is",mymap.getPixelOrigin())
+    let bounds = mymap.getPixelBounds();
+    let width = bounds.max.x - bounds.min.x;
+    let height = bounds.max.y - bounds.min.y;
+    console.log("origin is", mymap.getPixelOrigin());
     // create an svg
     let svg = d3.select(mymap.getPanes().overlayPane).append("svg");
-    svg.attr("height",height)
-    svg.attr("width",width)
+    svg.attr("height", height);
+    svg.attr("width", width);
     let g = svg.append("g").attr("class", "leaflet-zoom-hide");
     // create the geotransform
     let projectPoint = function(x, y) {
@@ -34,14 +47,18 @@
     });
     let d3path = d3.geoPath().projection(transform);
     // define apply latlng to layer that takes in geopoints and produces screenspace x,y for drawing on svg
+    // NOTE that geojson points are going to have the x first in the coordinates
     let applyLatLngToLayer = function(d) {
       // do some comparison of building number to geojson of buildings to get coords
-      let x = d[1];
       let y = d[0];
+      let x = d[1];
       return mymap.latLngToLayerPoint(new L.LatLng(y, x));
     };
     // create a point for the student union, i think this has been ordered y,x by lat lng
-    let dataPoint = [[32.232508, -110.951248]];
+    let dataPoint = [
+      [32.23261578999814, -110.95262080058234],
+      [32.225102, -110.95129]
+    ];
 
     // make a circle and append it to the svg, and then transform it with the results of the applylatlng
     let circle = g
@@ -55,8 +72,7 @@
         "transform",
         d => `translate(${applyLatLngToLayer(d).x},${applyLatLngToLayer(d).y})`
       )
-      .attr("fill","red")
-      ;
+      .attr("fill", "red");
   });
 </script>
 
