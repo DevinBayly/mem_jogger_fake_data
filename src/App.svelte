a0b2246eb96c03bad92f76286323e35352fb2784
Devin Bayly <baylyd@email.arizona.edu>
2020-06-29 17:00:30 -0700

    styled and placed first pass

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,8 @@
 <style>
 #holder {
   height:100%;
+    background:#183055;
+  color:white;
 }
 #title {
   display:flex;
@@ -15,6 +17,7 @@
 }
 #options {
   display:flex;
+  justify-content: center;
   height:100%;
 }
 </style>
@@ -25,10 +28,10 @@
   </div>
   <div id="options">
   <div id="gantt-side">
-    <VisOp imgSrc="./gantt_background.png" title="Gantt Chart" summaryText="View your data from the last 2 weeks with particular attention to the 'when' you were in a location"></VisOp>
+    <VisOp imgSrc="gantt_background.png" title="Gantt Chart" summaryText="View your data from the last 2 weeks with particular attention to the 'when' you were in a location"></VisOp>
   </div>
   <div id="map-side">
-  <VisOp imgSrc="./map_background.png" title="Map Visualization" summaryText="Explore where you've been on campus for over 15 minutes at a time overlaid on this campus map"></VisOp>
+  <VisOp imgSrc="map_background.png" title="Map Visualization" summaryText="Explore where you've been on campus for over 15 minutes at a time overlaid on this campus map"></VisOp>
   </div>
   </div>
 </div>
--- src/VisOption.svelte
+++ src/VisOption.svelte
@@ -1,29 +1,62 @@
 <script>
   export let imgSrc, title, summaryText;
+  import {onMount} from "svelte"
+  let imgElement
+  let holder
+  onMount(()=> {
+      // move the image into the top corner of the half
+      let holderRect = holder.getBoundingClientRect()
+    imgElement.style.position="absolute"
+    imgElement.style.top = holderRect.top+"px"
+    imgElement.style.left = holderRect.left + "px"
+
+  })
 </script>
 
 <style>
+* {
+    z-index: 5;
+}
 #optionHolder {
-    display:flex;
-    flex-direction: column;
-    align-items:center;
     height:100%;
 }
+#optionHolder img {
+    z-index: 1;
+}
+.text {
+    background:#183055;
+    color:white;
+    box-shadow: 0 7px 23px 0 rgba(32, 44, 62, 0.68);
+    border-radius: 10px;
+    margin:5%;
+}
 #title {
-    height:40%;
+    padding: 2%;
 }
 #summary{
     text-align:center;
 }
+#content {
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+    height:70%;
+    width:80%;
+    margin:0 auto;
+
+}
 </style>
 
 
-<div id="optionHolder">
-  <img src={imgSrc} alt="" id="background" />
-  <div id="title">
+<div id="optionHolder" bind:this={holder}>
+  <img bind:this={imgElement} src={imgSrc} alt="" id="background" />
+  <div id="content">
+  <div id="title" class="text">
     <h3>{title}</h3>
   </div>
-  <div id="summary">
+  <div id="summary" class="text">
     <p>{summaryText}</p>
+  </div>
   </div>
 </div>
