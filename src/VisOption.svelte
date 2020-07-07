bb8612ab377be03eb30f3b2464b61c3f40323842
DevinBayly <baylyd@email.arizona.edu>
2020-07-07 21:54:25 +0000

    include removal process, and style editing

--- src/VisOption.svelte
+++ src/VisOption.svelte
@@ -3,13 +3,9 @@
   import {onMount} from "svelte"
   let imgElement
   let holder
+  let content
   onMount(()=> {
-      // move the image into the top corner of the half
-      let holderRect = holder.getBoundingClientRect()
-    imgElement.style.position="absolute"
-    imgElement.style.top = holderRect.top+"px"
-    imgElement.style.left = holderRect.left + "px"
-
+    holder.style["background-image"] = `url(${imgSrc})`
   })
 </script>
 
@@ -22,6 +18,7 @@
 }
 #optionHolder img {
     z-index: 1;
+    width:200%;
 }
 .text {
     background:#183055;
@@ -44,14 +41,12 @@
     height:70%;
     width:80%;
     margin:0 auto;
-
 }
 </style>
 
 
 <div id="optionHolder" bind:this={holder}>
-  <img bind:this={imgElement} src={imgSrc} alt="" id="backgroundimage" />
-  <div id="content">
+  <div id="content" bind:this={content}>
   <div id="title" class="text">
     <h3>{title}</h3>
   </div>
