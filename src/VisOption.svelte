695e53041d3be203889241ef38dc0fb730fa48ea
DevinBayly <baylyd@email.arizona.edu>
2020-07-17 04:32:09 +0000

    merged wrong set of files last time

--- src/VisOption.svelte
+++ src/VisOption.svelte
@@ -20,12 +20,12 @@
     z-index: 1;
     width:200%;
 }
-.text {
-    background:#183055;
-    color:white;
+.textElement {
     box-shadow: 0 7px 23px 0 rgba(32, 44, 62, 0.68);
     border-radius: 10px;
     margin:5%;
+    padding:5%;
+    background:white;
 }
 #title {
     padding: 2%;
@@ -47,10 +47,10 @@
 
 <div id="optionHolder" bind:this={holder}>
   <div id="content" bind:this={content}>
-  <div id="title" class="text">
+  <div id="title" class="textElement">
     <h3>{title}</h3>
   </div>
-  <div id="summary" class="text">
+  <div id="summary" class="textElement">
     <p>{summaryText}</p>
   </div>
   </div>
