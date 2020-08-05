c394facbf3ddb02853039f4455aae9835d2e3378
DevinBayly <baylyd@email.arizona.edu>
2020-08-05 20:45:56 +0000

    make back button refresh page as easiest method

---
+++ src/backButton.svelte
@@ -0,0 +1,32 @@
+<script>
+export let visHolder
+import {onMount} from "svelte"
+import {bbstore,wifiData,subs} from "./store.js"
+import IV from "./IndividualVis.svelte"
+let holder
+
+let goBack= ()=> {
+location.reload()
+}
+</script>
+<style>
+#content {
+    display:flex;
+}
+#arrow{
+    font-size:30px;
+    margin:19px 0 20px;
+    color:white;
+}
+p {
+
+    margin:24px 0 20px;
+    color:white;
+}
+</style>
+<div id="content" bind:this={holder}>
+<div id="arrow" class="ua-brand-left-arrow" on:click={goBack} ></div>
+<div id="name">
+    <p >Memory Jog Map |</p>
+</div>
+</div>
--- src/map_vis.svelte
+++ src/map_vis.svelte
@@ -1,17 +1,24 @@
 <script>
 
+import {onMount} from "svelte"
+import BB from "./backButton.svelte"
 import Map from "./Map.svelte"
 import Brushable from "./brushableIndividual.svelte"
 import Selectors from "./dataSelectors.svelte"
 import {buildingJSON} from "./buildingData.js"
 import {wifiData } from "./store.js"
 // setup the store with the data for this application
-
+let visHolder
 let dims ={
   width:window.innerWidth*.5,
   height:100,
   margin:20
 }
+onMount(()=> {
+  new BB({
+    target:document.querySelector("#bbHolder"),
+  })
+})
 </script>
 
 <style>
@@ -28,7 +35,7 @@ let dims ={
 }
 </style>
 
-<div id="secondVisHolder">
+<div id="secondVisHolder" >
 <div id="topViews">
 <Selectors></Selectors>
 <Brushable dims={dims}></Brushable>
