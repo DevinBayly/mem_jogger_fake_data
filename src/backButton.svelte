53231f1ec4614ae470f6973fa09b6438a5dd92bf
DevinBayly <baylyd@email.arizona.edu>
2020-08-05 23:57:01 +0000

    include changes that make back button more obvious

--- src/backButton.svelte
+++ src/backButton.svelte
@@ -1,27 +1,40 @@
 <script>
-
-let goBack= ()=> {
-location.reload()
-}
+  let goBack = () => {
+    location.reload();
+  };
+  let arrowEle,pEle
+  let content
+    let entering = ()=> {
+        arrowEle.style.color = "black"
+        pEle.style.color = "black"
+    }
+    let leaving =()=> {
+        arrowEle.style.color = "white"
+        pEle.style.color = "white"
+    }
 </script>
-<style>
-#content {
-    display:flex;
-}
-#arrow{
-    font-size:30px;
-    margin:19px 0 20px;
-    color:white;
-}
-p {
 
-    margin:24px 0 20px;
-    color:white;
-}
+<style>
+  #content {
+    display: flex;
+  }
+  #arrow {
+    font-size: 30px;
+    margin: 19px 0 20px;
+    color: white;
+  }
+  p {
+    margin: 24px 0 20px;
+    color: white;
+  }
+  #content {
+    cursor: pointer;
+  }
 </style>
-<div id="content" >
-<div id="arrow" class="ua-brand-left-arrow" on:click={goBack} ></div>
-<div id="name">
-    <p >Memory Jog Map |</p>
-</div>
+
+<div id="content"  on:mouseenter={entering} on:mouseleave={leaving}>
+  <div bind:this={arrowEle} id="arrow" class="ua-brand-left-arrow" on:click={goBack} />
+  <div id="name">
+    <p bind:this={pEle}>Memory Jog Map |</p>
+  </div>
 </div>
