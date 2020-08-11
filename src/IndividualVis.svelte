d2055ce827a72f3f4cbe40d3197ecf5f0bf27bd5
DevinBayly <baylyd@email.arizona.edu>
2020-08-11 18:21:24 +0000

    update intro page to 1/3 2/3 arrangement

--- src/IndividualVis.svelte
+++ src/IndividualVis.svelte
@@ -49,38 +49,60 @@
     justify-content: center;
     height: 100%;
   }
-  #MJMexplanation p {
+  #subIntro p {
     width: 80%;
     margin: 0 auto;
+    padding-bottom:20px;
+  }
+  #horizontal {
+    display: flex;
+    align-items:center;
+  }
+  #lhs {
+    flex:1 1;
+
+  }
+  #rhs {
+    flex:3 3 ;
   }
 </style>
 
 <div id="holder" bind:this={holder}>
   <div id="title">
     <h3>Memory Jogger</h3>
+  </div>
+  <div id="subIntro">
 
+        <p>
+          This Memory Jog map is designed to help you remember where you’ve been
+          on the UA campus for the two weeks prior to your survey. Please
+          understand that the displayed locations and other data is a “best
+          guess”, based on UA wifi connection information from your
+          internet-enabled devices, such as a smart watch, or mobile phone. The
+          connection information can be ambiguous, and vary widely from one
+          smart device to another. This tool is only to help jog your memory,
+          and is not guaranteed to be completely accurate.
+        </p>
   </div>
   <div id="MJMexplanation">
-    <p>
-      This Memory Jog map is designed to help you remember where you’ve been on
-      the UA campus for the two weeks prior to your survey. Please understand
-      that the displayed locations and other data is a “best guess”, based on UA
-      wifi connection information from your internet-enabled devices, such as a
-      smart watch, or mobile phone. The connection information can be ambiguous,
-      and vary widely from one smart device to another. This tool is only to
-      help jog your memory, and is not guaranteed to be completely accurate.
-    </p>
-  </div>
-  <div id="screencast">
-    <a href="instructions.webm">"How to use" video</a>
-  </div>
-  <div id="options">
-    <div id="map-side" on:click={map}>
-      <VisOp
-        imgSrc="map_background_updated.png"
-        title="Map Visualization"
-        summaryText="Click the image to explore where you've been on campus for
-        over 15 minutes at a time overlaid on this campus map" />
+    <div id="horizontal">
+      <div id="lhs">
+        <div id="screencast">
+          <a href="instructions.webm">"How to use" video</a>
+        </div>
+      </div>
+      <div id="rhs">
+        <div id="options">
+          <div id="map-side" on:click={map}>
+            <VisOp
+              imgSrc="map_background_updated.png"
+              title="Map Visualization"
+              summaryText="Click the image to explore where you've been on
+              campus for over 15 minutes at a time overlaid on this campus map" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
+
 </div>
