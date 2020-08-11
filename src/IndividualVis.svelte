94aaf21cae9a4d46664cbc67e61d51750642e07c
DevinBayly <baylyd@email.arizona.edu>
2020-08-11 18:06:47 +0000

    make removal of intro on cookie map load

--- src/IndividualVis.svelte
+++ src/IndividualVis.svelte
@@ -1,33 +1,35 @@
 <script>
   export let reportData;
   import VisOp from "./VisOption.svelte";
+  import { onMount } from "svelte";
   import MapApplication from "./map_vis.svelte";
   // put in Teresa's signin code
   // include the reachout to the lambda function
   // introduce the page with the options
   let holder;
   let map = () => {
-  for (let opHolder of document.querySelectorAll("#holder")) {
-    opHolder.remove();
-  }
+    for (let opHolder of document.querySelectorAll("#holder")) {
+      opHolder.remove();
+    }
     console.log("loading map");
     // remove holder
     new MapApplication({
       target: document.body
     });
-  }
-  // look for the gotovis ccookie, and launch map automatically if found
-  let cookies = decodeURIComponent(document.cookie)
-  cookies.split(";").map(e=> {
-    console.log("searching cookies",e)
-    if (e.match(/gotovis=true/)){
-      // remove the cookie
-      document.cookie="gotovis=false"
-      // launch the map
-      map()
-    }
-  })
-  ;
+  };
+  onMount(() => {
+    // look for the gotovis ccookie, and launch map automatically if found
+    let cookies = decodeURIComponent(document.cookie);
+    cookies.split(";").map(e => {
+      console.log("searching cookies", e);
+      if (e.match(/gotovis=true/)) {
+        // remove the cookie
+        document.cookie = "gotovis=false";
+        // launch the map
+        map();
+      }
+    });
+  });
 </script>
 
 <style>
@@ -48,11 +50,9 @@
     height: 100%;
   }
   #MJMexplanation p {
-    width:80%;
-    margin:0 auto;
-
+    width: 80%;
+    margin: 0 auto;
   }
-
 </style>
 
 <div id="holder" bind:this={holder}>
@@ -61,16 +61,26 @@
 
   </div>
   <div id="MJMexplanation">
-  <p>This Memory Jog map is designed to help you remember where you’ve been on the UA campus for the two weeks prior to your survey. Please understand that the displayed locations and other data is a “best guess”, based on UA wifi connection information from your internet-enabled devices, such as a smart watch, or mobile phone. The connection information can be ambiguous, and vary widely from one smart device to another. This tool is only to help jog your memory, and is not guaranteed to be completely accurate.</p></div>
+    <p>
+      This Memory Jog map is designed to help you remember where you’ve been on
+      the UA campus for the two weeks prior to your survey. Please understand
+      that the displayed locations and other data is a “best guess”, based on UA
+      wifi connection information from your internet-enabled devices, such as a
+      smart watch, or mobile phone. The connection information can be ambiguous,
+      and vary widely from one smart device to another. This tool is only to
+      help jog your memory, and is not guaranteed to be completely accurate.
+    </p>
+  </div>
   <div id="screencast">
-  <a href="instructions.webm">"How to use" video</a></div>
+    <a href="instructions.webm">"How to use" video</a>
+  </div>
   <div id="options">
     <div id="map-side" on:click={map}>
       <VisOp
         imgSrc="map_background_updated.png"
         title="Map Visualization"
-        summaryText="Click the image to explore where you've been on campus for over 15 minutes at
-        a time overlaid on this campus map" />
+        summaryText="Click the image to explore where you've been on campus for
+        over 15 minutes at a time overlaid on this campus map" />
     </div>
   </div>
 </div>
